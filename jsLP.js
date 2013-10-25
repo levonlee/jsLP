c78f17c6e21cfc00a4ef9de675677096b0adee02
levonlee <levonlee@gmail.com>
2013-10-25 11:31:29 -0400

    New function LPUpdateQueryString

--- jsLP.js
+++ jsLP.js
@@ -37,6 +37,38 @@ function stringToSearchQuery (s,d) {
 	return s.replace(/\s+/g, d);
 	// stringToSearchQuery('I love you', '+AND+') returns I+AND+love+AND+you
 }
+function LPUpdateQueryString(key, value, url) {
+	if (!url) url = window.location.href;
+	var re = new RegExp("([?|&])" + key + "=.*?(&|#|$)(.*)", "gi");
+
+	if (re.test(url)) {
+		if (typeof value !== 'undefined' && value !== null)
+				return url.replace(re, '$1' + key + "=" + value + '$2$3');
+		else {
+			var hash = url.split('#');
+			url = hash[0].replace(re, '$1$3').replace(/(&|\?)$/, '');
+			if (typeof hash[1] !== 'undefined' && hash[1] !== null) 
+					url += '#' + hash[1];
+			return url;
+		}
+	}
+	else {
+		if (typeof value !== 'undefined' && value !== null) {
+			var separator = url.indexOf('?') !== -1 ? '&' : '?',
+					hash = url.split('#');
+			url = hash[0] + separator + key + '=' + value;
+			if (typeof hash[1] !== 'undefined' && hash[1] !== null) 
+					url += '#' + hash[1];
+			return url;
+		}
+		else
+			return url;
+	}
+	// Add a query string parameter if it doesn't exist. Don't submit value means 
+	// to get rid of the parameter. Thanks for ellemayo for providing this function
+	// http://stackoverflow.com/questions/5999118/add-or-update-query-string-parameter
+	// e.g. LPUpdateQueryString('aUrlVar','123','/index.php?a=1&aUrlVar=2') changes aUrlVar to 123.
+}
 function LPlnkAndTxt(c,l) {
 	var cl = l.trim();
 	if (cl !=='') { 
